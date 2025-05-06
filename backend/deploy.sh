136e5c6eb9a63381e27300ec6fc89f070d5f1bed
aeretnov <eretnov@gmail.com>
2025-05-06 21:55:03 +0500

    Remove old version from curl

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -3,7 +3,7 @@
 set -xe
 sudo cp -rf backend.service /etc/systemd/system/backend.service
 sudo rm -f /opt/sausage-store/bin/sausage-store.jar||true
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/0.0.1-SNAPSHOT/${VERSION}/sausage-store-${VERSION}.jar
+curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 sudo cp ./sausage-store.jar /opt/sausage-store/bin/sausage-store.jar||true
 sudo systemctl daemon-reload
 sudo systemctl restart backend
