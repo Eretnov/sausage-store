a95d36a53551461d2b92f0925214b66dec885650
aeretnov <eretnov@gmail.com>
2025-05-07 19:48:47 +0500

    Fix deploy script

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -3,6 +3,6 @@
 set -xe
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.tar.gz ${NEXUS_REPO_URL}/${NEXUS_REPO_FRONTEND_NAME}/${VERSION}/sausage-store-${VERSION}.tar.gz
 sudo rm -rf /opt/sausage-store/static/*
-tar -xzf ./sausage-store.tar.gz -C /opt/sausage-store/static/ || true
+sudo tar -xzfv ./sausage-store.tar.gz -C /opt/sausage-store/static/ && rm -f ./sausage-store.tar.gz
 sudo chown -R frontend: /opt/sausage-store/static/
 sudo systemctl reload nginx
