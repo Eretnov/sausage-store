61be57f027044a78c6d30fdbf15f72935b505349
std-040-31 std-040-31 <std-040-31@praktikum-services.ru>
2025-05-11 10:38:40 +0000

    Resolve "Deploy App"

---
+++ backend/deploy.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+set -xe
+sudo cp -rf backend.service /etc/systemd/system/backend.service
+sudo rm -f /opt/sausage-store/bin/sausage-store.jar||true
+curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
+sudo cp ./sausage-store.jar /opt/sausage-store/bin/sausage-store.jar && rm -f ./sausage-store.jar
+sudo systemctl daemon-reload
+sudo systemctl restart backend
