0ebcfb9b32f3b02b126a2aed4fcc926652720bae
std-040-31 std-040-31 <std-040-31@praktikum-services.ru>
2025-06-30 21:14:50 +0000

    Vault

--- backend/deploy_template.sh
+++ backend/deploy_template.sh
@@ -4,6 +4,7 @@ sudo docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTR
 sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-backend || true
 sudo docker run --rm -d --name sausage-backend \
+     --env VAULT_TOKEN="${VAULT_TOKEN}" \
      --env SPRING_DATASOURCE_URL="${SPRING_DATASOURCE_URL}" \
      --env SPRING_DATASOURCE_USERNAME="${SPRING_DATASOURCE_USERNAME}" \
      --env SPRING_DATASOURCE_PASSWORD="${SPRING_DATASOURCE_PASSWORD}" \
